0840167b65eecb7da89822bb293b25f5b02d2c10
Bytes <bytes111@gmail.com>
2024-09-01 12:01:08 +0300

    Add new plugins for neovim

---
+++ .config/nvim/lua/custom/plugins/tailwindcss-tools.lua
@@ -0,0 +1,45 @@
+-- tailwind-tools.lua
+return {
+  'luckasRanarison/tailwind-tools.nvim',
+  name = 'tailwind-tools',
+  build = ':UpdateRemotePlugins',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-telescope/telescope.nvim', -- optional
+    'neovim/nvim-lspconfig', -- optional
+  },
+  opts = {
+    document_color = {
+      enabled = true, -- can be toggled by commands
+      kind = 'inline', -- "inline" | "foreground" | "background"
+      inline_symbol = '󰝤 ', -- only used in inline mode
+      debounce = 200, -- in milliseconds, only applied in insert mode
+    },
+    conceal = {
+      enabled = false, -- can be toggled by commands
+      min_length = nil, -- only conceal classes exceeding the provided length
+      symbol = '󱏿', -- only a single character is allowed
+      highlight = { -- extmark highlight options, see :h 'highlight'
+        fg = '#38BDF8',
+      },
+    },
+    cmp = {
+      highlight = 'foreground', -- highlight either bg or fg of text in color previews in cmp menu. can be "foreground" | "background"
+    },
+    telescope = {
+      utilities = {
+        -- the function used when selecting an utility class in telescope
+        callback = function(name, class) end,
+      },
+    },
+    -- see the extension section to learn more
+    extension = {
+      queries = {}, -- a list of filetypes having custom `class` queries
+      patterns = { -- a map of filetypes to Lua pattern lists
+        -- exmaple:
+        -- rust = { "class=[\"']([^\"']+)[\"']" },
+        -- javascript = { "clsx%(([^)]+)%)" },
+      },
+    },
+  },
+}
